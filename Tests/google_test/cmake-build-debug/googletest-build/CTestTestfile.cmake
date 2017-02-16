fa52facbc2422bf850f29ad866994cefe32243ac
VvWijhe <victor.v.wijhe@hotmail.com>
2017-02-16 15:04:44 +0100

    google test?

--- Tests/google_test/cmake-build-debug/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/victor/harde schijf/school/HBO2/semester_2/Female-Replacement/Tests/google_test/cmake-build-debug/googletest-src
-# Build directory: /media/victor/harde schijf/school/HBO2/semester_2/Female-Replacement/Tests/google_test/cmake-build-debug/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(googlemock)
