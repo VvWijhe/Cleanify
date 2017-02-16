aec3a0d5cfa016c1ab9ea36d3c2c071095f015f6
VvWijhe <victor.v.wijhe@hotmail.com>
2017-02-16 14:10:30 +0100

    google test?

---
+++ Tests/google_test/cmake-build-debug/CMakeFiles/google_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/google_test.dir/main.cpp.o"
+  "google_test.pdb"
+  "google_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/google_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
