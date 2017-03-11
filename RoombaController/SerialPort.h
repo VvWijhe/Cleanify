de49a4924f7829f7bc25bbf57ea5e39da491f15a
Raymon <r.dinter.rvd@gmail.com>
2017-03-11 17:18:39 +0100

    Begin gemaakt aan serialport skelet

---
+++ RoombaController/SerialPort.h
@@ -0,0 +1,18 @@
+//
+// Created by raymon on 11-3-17.
+//
+
+#ifndef FEMALE_REPLACEMENT_SERIALPORT_H
+#define FEMALE_REPLACEMENT_SERIALPORT_H
+
+class SerialPort{
+private:
+
+public:
+    void connect(void);
+    void disconnect(void);
+    void write(unsigned char cmd);
+    void read(void);
+};
+
+#endif //FEMALE_REPLACEMENT_SERIALPORT_H
---
+++ RoombaController/main.cpp
@@ -0,0 +1,9 @@
+#include "SerialPort.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+}
