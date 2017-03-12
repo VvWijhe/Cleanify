1e71a22103f5460696fd22a934b7e65eba025f4d
jamiecheng97@gmail.com <jamiecheng97@gmail.com>
2017-03-12 18:48:40 +0100

    updated folderstructure

---
+++ RoombaController/cmake-build-debug/CMakeFiles/RoombaController.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RoombaController.dir/Dataframe/DataFrames.cpp.o"
+  "CMakeFiles/RoombaController.dir/serial/SerialPort.cpp.o"
+  "CMakeFiles/RoombaController.dir/main.cpp.o"
+  "RoombaController.pdb"
+  "RoombaController"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RoombaController.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RoombaController/cmake-build-debug/Dataframe/CMakeFiles/Dataframes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dataframes.dir/DataFrames.cpp.o"
+  "Dataframes.pdb"
+  "Dataframes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dataframes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
