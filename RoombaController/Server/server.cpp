1c24531e5c5645621f5f25e26e0af4c77be2c3ed
jamiecheng97@gmail.com <jamiecheng97@gmail.com>
2017-03-15 14:14:46 +0100

    added http server

---
+++ RoombaController/Server/server.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jamie on 3/15/17.
+//
+
+#include "server.h"
+
+HttpServer::HttpServer() {
+
+}
+
+HttpServer::~HttpServer() {
+
+}
---
+++ RoombaController/Server/server.h
@@ -0,0 +1,19 @@
+//
+// Created by jamie on 3/15/17.
+//
+
+#ifndef ROOMBACONTROLLER_SERVER_H
+#define ROOMBACONTROLLER_SERVER_H
+
+#include "http_server.hpp"
+
+class HttpServer {
+public:
+    HttpServer();
+
+    ~HttpServer();
+private:
+
+};
+
+#endif //ROOMBACONTROLLER_SERVER_H
