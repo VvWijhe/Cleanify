2d12f248398a1251664584870d11813d15e1e0a4
jamiecheng97@gmail.com <jamiecheng97@gmail.com>
2017-03-16 19:55:23 +0100

    added sample tests

---
+++ RoombaController/Tests/tests/roombatests.cpp
@@ -0,0 +1,14 @@
+//
+// Created by victor on 13.03.17.
+//
+
+#include "gtest/gtest.h"
+
+TEST(basic_test, test_eq) {
+    EXPECT_EQ(1, 0);
+}
+
+TEST(basic_test, testn_eq) {
+    EXPECT_NE(1, 1);
+}
+
