ab2bed539af3e176bb956bb9e1c32895c558c269
jamiecheng97@gmail.com <jamiecheng97@gmail.com>
2017-03-29 10:40:27 +0200

    clean main

--- RoombaController/main.cpp
+++ RoombaController/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-#include "Server/server.h"
-#include "io/roombacontroller.h"
 
 using namespace std;
 
 int main() {
-    io::RoombaSCI sci("port", 109200);
-    io::roombacontroller control(sci);
-    //server::RoombaServer roombaServer(8080);
-    //roombaServer.run();
-    //cin.ignore();
-    //roombaServer.stop();
-
-    control.readSensors();
-
     return 0;
 }
