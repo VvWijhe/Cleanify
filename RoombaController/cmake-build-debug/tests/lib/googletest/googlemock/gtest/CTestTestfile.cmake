4f658c1125c7855a57fb80c37cab9d0f03edc4f6
Jamie Cheng <jamiecheng97@gmail.com>
2017-04-18 14:33:31 +0200

    removed cmake build debug folder

--- RoombaController/cmake-build-debug/tests/lib/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bas/CLionProjects/Cleanify/RoombaController/tests/lib/googletest/googletest
-# Build directory: /home/bas/CLionProjects/Cleanify/RoombaController/cmake-build-debug/tests/lib/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
