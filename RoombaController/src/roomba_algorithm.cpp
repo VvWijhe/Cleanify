f3e0044eea8103ed9e1d1fd02e7d368c6066dc88
Raymon <r.dinter.rvd@gmail.com>
2017-05-02 15:22:42 +0200

    cmake

--- RoombaController/src/roomba_algorithm.cpp
+++ RoombaController/src/roomba_algorithm.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "roomba_algorithm.h"
+
+using namespace algorithm;
+
+systemcontrol::RoombaControl::parameters Clean::calculate(systemcontrol::RoombaControl::parameters p, double dt) {
+    switch (currentState) {
+        case S_START:
+            currentState = S_SPIRAL;
+            break;
+        case S_SPIRAL:
+            if(/*bumped*/ 1) {
+                currentState = S_DRIVE_BACKWARDS;
+                eventOut = E_HIT_OBJECT;
+            }
+            else {
+                //set roomba parameters
+            }
+            break;
+
+        case S_DRIVE_BACKWARDS:
+            currentState = S_ROTATE_LEFT;
+            //eventOut = E_BACKWARDS_DONE;
+            break;
+
+        case S_ROTATE_LEFT:
+            currentState = S_DRIVE_STRAIGT;
+            //eventOut = E_ROTATE_DONE;
+            break;
+
+        case S_FOLLOW_WALL:
+            if(/*bumped*/ ) {
+                currentState = S_DRIVE_BACKWARDS;
+                eventOut = E_HIT_OBJECT;
+            }
+            else if( /*time exceeded*/ ){
+
+            }
+            else{
+                //set roomba parameters
+            }
+            break;
+
+        case S_BIG_ROTATE_LEFT:
+
+        case S_DRIVE_STRAIGT:
+            // if hit wall
+            currentState = S_FOLLOW_WALL;
+            //if time exceeded
+            currentState = S_SPIRAL;
+            break;
+
+        default:
+            //return eventOut;
+            break;
+        case S_NO:break;
+    }
+}
--- RoombaController/src/roomba_algorithm.h
+++ RoombaController/src/roomba_algorithm.h
@@ -5,11 +5,54 @@
 #ifndef CLEANIFY_ROOMBA_ALGORITHM_H
 #define CLEANIFY_ROOMBA_ALGORITHM_H
 
+#include "roomba_control.h"
+
 namespace algorithm {
-
     class roomba_algorithm {
+    public:
+        using parameters = systemcontrol::RoombaControl::parameters;
 
+        roomba_algorithm();
+
+        ~roomba_algorithm() = default;
+
+        virtual parameters calculate(parameters p, double dt) = 0;
     };
 
+    class Clean : public roomba_algorithm {
+    public:
+        using parameters = systemcontrol::RoombaControl::parameters;
+        typedef enum {
+            S_NO, S_START, S_SPIRAL, S_FOLLOW_WALL, S_DRIVE_BACKWARDS, S_ROTATE_LEFT, S_BIG_ROTATE_LEFT, S_DRIVE_STRAIGT
+        } state_e;
+        typedef enum {
+            E_NO, E_READY, E_HIT_OBJECT, E_TIME_EXCEEDED
+        } event_e;
+
+        Clean() : currentState(S_START), eventOut(E_NO) {}
+
+        ~Clean() = default;
+
+        parameters calculate(parameters p, double dt);
+
+        state_e getCurrentState() const { return currentState; }
+
+    private:
+        state_e currentState;
+        event_e eventOut;
+    };
+
+    class Spot : public roomba_algorithm {
+    public:
+        using parameters = systemcontrol::RoombaControl::parameters;
+        typedef enum {
+        } state_e;
+        typedef enum {
+        } event_e;
+
+        parameters calculate(parameters p, double dt);
+
+
+    };
 }
 #endif //CLEANIFY_ROOMBA_ALGORITHM_H
