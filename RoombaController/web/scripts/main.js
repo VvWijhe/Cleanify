bba9e6debd388b8f0c2f0a70db68334d5bfa8cdd
VvWijhe <victor.v.wijhe@hotmail.com>
2017-05-09 12:54:05 +0200

    added seperate .js files for diff .html

--- RoombaController/web/scripts/main.js
+++ RoombaController/web/scripts/main.js
@@ -2,41 +2,6 @@
  * Created by victor on 04.05.17.
  */
 
-var myVar = setInterval(myTimer, 500);
-
-
-$('#slider_motor').slider({
-    tooltip_position: 'bottom',
-    formatter: function (value) {
-        return `Motors are on : ${Number((value * 100).toFixed(2))}%`;
-
-    }
-});
-$('#slider_brush').slider({
-    tooltip_position: 'bottom',
-    formatter: function (value) {
-        return 'Brushes are on : ' + Number((value * 100).toFixed(2)) + '%';
-    }
-});
-
-
-function myTimer() {
-    $.get("/status", function (data) {
-        let progress_bar = $("#progress-bar");
-        let obj_status = JSON.parse(data);
-        progress_bar.css("width", (obj_status.battery) + '%');
-        document.getElementById("progress_number").textContent = ( obj_status.battery + '%');
-        progress_bar.attr('class', 'progress-bar progress-bar-striped active');
-        if (obj_status.battery <= 25) {
-            progress_bar.attr('class', 'progress-bar progress-bar-striped active progress-bar-warning');
-            if (obj_status.battery <= 10) {
-                progress_bar.attr('class', 'progress-bar progress-bar-striped active progress-bar-danger');
-            }
-        }
-    });
-}
-
-
 $(document).ready(function () {
     $(".dir").click(function () {
         let form = "{\"direction\" : \"" + this.id + "\", \"session\" : \"webapp\"}"
@@ -83,16 +48,3 @@ $(document).ready(function () {
     });
 });
 
-function myMap() {
-    let Cleanify = {lat: 51.9889801, lng: 5.949402};
-    let mapCanvas = document.getElementById("map");
-    let mapOptions = {
-        center: new google.maps.LatLng(Cleanify), zoom: 16,
-        scrollwheel: false
-    };
-    let map = new google.maps.Map(mapCanvas, mapOptions);
-    let marker = new google.maps.Marker({
-        position: Cleanify,
-        map: map
-    });
-}
