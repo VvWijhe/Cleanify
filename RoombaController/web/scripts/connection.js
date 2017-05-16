08d247fb25ba48c5bd3cd3769cda6caf1b731945
VvWijhe <victor.v.wijhe@hotmail.com>
2017-05-16 20:31:42 +0200

    moved connection from manual to connection

--- RoombaController/web/scripts/connection.js
+++ RoombaController/web/scripts/connection.js
@@ -1,3 +1,34 @@
 /**
  * Created by victor on 11.05.17.
  */
+
+var timerVar;
+var connected = false;
+
+function Connect() {
+    let form = "{\"direction\" : \"" + "stop" + "\", \"session\" : \"webapp\"}";
+
+    $.get("/status", function (data) {
+        let obj_status = JSON.parse(data);
+
+        if (obj_status.status === "available") {
+            timerVar = setInterval(myTimer, 500);
+            connected = true;
+            $("#manual_panel").attr('class', 'panel panel-success');
+            $("#autonomous_panel").attr('class', 'panel panel-success');
+            $.post("/control",
+                form,
+                function (data) {
+                    console.log(data);
+                }, "text").fail(function (jqXHR, textStatus, errorThrown) {
+                alert("ERROR: NO CONNECTION");
+            });
+        }
+        else if (obj_status.status === "busy") {
+            connected = false;
+            $("#occupied").show();
+            $("#manual_panel").attr('class', 'panel panel-danger');
+            $("#autonomous_panel").attr('class', 'panel panel-danger')
+        }
+    });
+}
--- RoombaController/web/scripts/manual.js
+++ RoombaController/web/scripts/manual.js
@@ -1,34 +1,7 @@
-var timerVar;
-var connected = false;
+
 let progress_bar = $("#progress-bar");
 
-function Connect() {
-    let form = "{\"direction\" : \"" + "stop" + "\", \"session\" : \"webapp\"}";
 
-    $.get("/status", function (data) {
-        let obj_status = JSON.parse(data);
-
-        if (obj_status.status === "available") {
-            timerVar = setInterval(myTimer, 500);
-            connected = true;
-            $("#manual_panel").attr('class', 'panel panel-success');
-            $("#autonomous_panel").attr('class', 'panel panel-success');
-            $.post("/control",
-                form,
-                function (data) {
-                    console.log(data);
-                }, "text").fail(function (jqXHR, textStatus, errorThrown) {
-                alert("ERROR: NO CONNECTION");
-            });
-        }
-        else if (obj_status.status === "busy") {
-            connected = false;
-            $("#occupied").show();
-            $("#manual_panel").attr('class', 'panel panel-danger');
-            $("#autonomous_panel").attr('class', 'panel panel-danger')
-        }
-    });
-}
 
 function Disconnect() {
     let form = "{\"exit\" : \"true\"}";
