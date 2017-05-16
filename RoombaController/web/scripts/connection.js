282fd1378b70046ebed6b67ec8b7d8b166e696b6
VvWijhe <victor.v.wijhe@hotmail.com>
2017-05-16 20:51:17 +0200

    changed connect function

--- RoombaController/web/scripts/connection.js
+++ RoombaController/web/scripts/connection.js
@@ -2,33 +2,36 @@
  * Created by victor on 11.05.17.
  */
 
-var timerVar;
-var connected = false;
+let varManual;
+let varAuto;
+let connected = false;
 
 function Connect() {
     let form = "{\"direction\" : \"" + "stop" + "\", \"session\" : \"webapp\"}";
 
     $.get("/status", function (data) {
-        let obj_status = JSON.parse(data);
+            let obj_status = JSON.parse(data);
 
-        if (obj_status.status === "available") {
-            //timerVar = setInterval(myTimer, 500);
-            connected = true;
-            $("#manual_panel").attr('class', 'panel panel-success');
-            $("#autonomous_panel").attr('class', 'panel panel-success');
-            $.post("/control",
-                form,
-                function (data) {
-                    console.log(data);
-                }, "text").fail(function (jqXHR, textStatus, errorThrown) {
-                alert("ERROR: NO CONNECTION");
-            });
+            //if the site is available
+            if (obj_status.status === "available") {
+                varManual = setInterval(timerManual, 500);
+                varAuto = setInterval(timerAuto, 500);
+                connected = true;
+                $.post("/control",
+                    form,
+                    function (data) {
+                        console.log(data);
+                    }, "text").fail(function (jqXHR, textStatus, errorThrown) {
+                    alert("ERROR: NO CONNECTION");
+                });
+            }
+            // if the server is busy and the site is not connected
+            else if (obj_status.status === "busy" && connected === false) {
+                $("#occupied").show();
+                $("#manual_panel").attr('class', 'panel panel-danger');
+                $("#autonomous_panel").attr('class', 'panel panel-danger')
+            }
         }
-        else if (obj_status.status === "busy") {
-            connected = false;
-            $("#occupied").show();
-            $("#manual_panel").attr('class', 'panel panel-danger');
-            $("#autonomous_panel").attr('class', 'panel panel-danger')
-        }
-    });
+    )
+    ;
 }
